41d76adf3be774909cbc5d416861bf17df66b1f2
dunowljj <uuufg3322@gmail.com>
2022-07-05 15:22:23 +0900

    refactor : InputView, ResultView의 메서드 모두 static메서드로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,11 +2,11 @@ package racingcar.view;
 
 public class InputView {
 
-    private void requireNames() {
+    public static void printRequireNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
 
-    private void requireRepeat() {
+    public static void printRequireRepeat() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 }
--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -6,27 +6,28 @@ import racingcar.domain.Cars;
 
 public class ResultView {
 
-    public void printTitle() {
+    public static void printTitle() {
         System.out.println("실행 결과");
     }
 
-    public void printRace(Cars cars) {
+    public static void printRace(Cars cars) {
         for (Car car : cars.getCarsList()) {
             printName(car);
             printPosition(car);
         }
         System.out.println();
     }
-    private void printName(Car car) {
+    private static void printName(Car car) {
         System.out.print(car.getName()+" : ");
     }
-    private void printPosition(Car car) {
+    private static void printPosition(Car car) {
         for (int i = 0; i < car.getPosition(); i++) {
             System.out.print("-");
         }
+        System.out.println();
     }
 
-    public void printWinner(String names) {
-        System.out.print(names+"가 최종 우승했습니다.");
+    public static void printWinner(String names) {
+        System.out.print(names+"(이)가 최종 우승했습니다.");
     }
 }
